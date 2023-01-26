80e6a735cc403f1748b3a9716e1be575093a2cfd
MonakhovA <MonGraph67>
2023-01-26 20:21:48 +0300

    commit

--- src/main/java/ru/hogwarts/school/controller/StudentController.java
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -1,15 +1,35 @@
 package ru.hogwarts.school.controller;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Collections;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
 import ru.hogwarts.school.service.StudentService;
 
-import java.util.Collection;
-
-@RequestMapping("student")
 @RestController
+@RequestMapping("/student")
 public class StudentController {
 
     private final StudentService studentService;
@@ -18,19 +38,21 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    @PostMapping
-    public Student create(@RequestBody Student student){
-        return studentService.create(student);
+    @GetMapping("{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable Long id) {
+        Student student = studentService.findStudent(id);
+        if (student == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
     }
 
-    @GetMapping("{id}")
-    public ResponseEntity<Student> read(@PathVariable Long id){
-        Student student = studentService.read(id);
-        if(student==null){
-            return ResponseEntity.notFound().build();
-        }else {
-            return ResponseEntity.ok(student);
+    @GetMapping("/filter/{age}")
+    public ResponseEntity<Collection<Student>> filterByAge(@RequestParam(required = false) int age) {
+        if (age > 0) {
+            return ResponseEntity.ok(studentService.filterByAge(age));
         }
+        return ResponseEntity.ok(Collections.emptyList());
     }
 
     @GetMapping
@@ -51,37 +73,67 @@ public class StudentController {
         }
     }
 
-
-
     @GetMapping("/faculty/{studentId}")
-    public ResponseEntity<Faculty> getFaculty(@PathVariable Long studentId){
+    public ResponseEntity<Faculty> getFaculty(@PathVariable long studentId){
         //получить факультет студента
-        return ResponseEntity.ok(studentService.read(studentId).getFaculty());
+        return ResponseEntity.ok(studentService.findStudent(studentId).getFaculty());
     }
 
 
-
-
-    @GetMapping("/filter/{age}")
-    public ResponseEntity<Collection<Student>> filterByAge(@PathVariable Integer age){
-        return ResponseEntity.ok(studentService.filterByAge(age));
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
     }
-
 
     @PutMapping
-    public ResponseEntity<Student> update(@RequestBody Student student){
-        Student updateStudent = studentService.update(student);
-        if(updateStudent==null){
-            return ResponseEntity.notFound().build();
-        }else {
-            return ResponseEntity.ok(updateStudent);
+    public ResponseEntity<Student> editStudent(@RequestBody Student student) {
+        Student foundStudent = studentService.editStudent(student);
+        if (foundStudent == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
+        return ResponseEntity.ok(foundStudent);
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity delete(@PathVariable Long id){
-        studentService.delete(id);
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
         return ResponseEntity.ok().build();
     }
 
+
+    @PostMapping(value = "/{id}/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> uploadAvatar(@PathVariable Long id, @RequestParam MultipartFile avatar) throws IOException {
+        if (avatar.getSize() > 1024 * 300) {
+            return ResponseEntity.badRequest().body("File is too big");
+        }
+
+        studentService.uploadAvatar(id, avatar);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/{id}/avatar/preview")
+    public ResponseEntity<byte[]> downloadAvatar(@PathVariable Long id) {
+        Avatar avatar = studentService.findAvatar(id);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType(avatar.getMediaType()));
+        headers.setContentLength(avatar.getData().length);
+
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(avatar.getData());
+    }
+
+    @GetMapping(value = "/{id}/avatar")
+    public void downloadAvatar(@PathVariable Long id, HttpServletResponse response) throws IOException {
+        Avatar avatar = studentService.findAvatar(id);
+
+        Path path = Path.of(avatar.getFilePath());
+
+        try (InputStream is = Files.newInputStream(path);
+             OutputStream os = response.getOutputStream();) {
+            response.setStatus(200);
+            response.setContentType(avatar.getMediaType());
+            response.setContentLength((int) avatar.getFileSize());
+            is.transferTo(os);
+        }
+    }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,37 +1,50 @@
 package ru.hogwarts.school.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import ru.hogwarts.school.model.Faculty;
+import org.springframework.web.multipart.MultipartFile;
+import ru.hogwarts.school.model.Avatar;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repository.AvatarRepository;
 import ru.hogwarts.school.repository.StudentRepository;
-
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collection;
-import java.util.stream.Collectors;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @Service
 public class StudentService {
 
+    @Value("${avatars.dir.path}")
+    private String avatarsDir;
+
     private final StudentRepository studentRepository;
+    private final AvatarRepository avatarRepository;
 
-    public StudentService(StudentRepository studentRepository) {
+    public StudentService(StudentRepository studentRepository, AvatarRepository avatarRepository) {
         this.studentRepository = studentRepository;
+        this.avatarRepository = avatarRepository;
     }
 
-    public Student create(Student student){
+    public Student addStudent(Student student) {
+        student.setId(null);
         return studentRepository.save(student);
     }
 
-    public Student read(long id){
-        return studentRepository.findById(id).get();
+    public Student findStudent(long id) {
+        return studentRepository.findById(id).orElseThrow();
     }
 
-    public Student update(Student student){
+    public Student editStudent(Student student) {
         return studentRepository.save(student);
     }
 
-    public void delete(long id) {
+    public void deleteStudent(long id) {
         studentRepository.deleteById(id);
     }
+
 
     public Collection<Student> readAll() {
         return studentRepository.findAll();
@@ -41,8 +54,41 @@ public class StudentService {
         return studentRepository.findByAge(age);
     }
 
-    public Collection<Student> findByAgeBetween(int min, int max ) {
+    public Collection<Student> findByAgeBetween(int min, int max) {
         return studentRepository.findByAgeBetween(min, max);
     }
 
+
+    public Avatar findAvatar(long studentId) {
+        return avatarRepository.findByStudentId(studentId).orElseThrow();
+    }
+
+    public void uploadAvatar(Long studentId, MultipartFile file) throws IOException {
+        Student student = findStudent(studentId);
+
+        Path filePath = Path.of(avatarsDir, studentId + "." + getExtension(file.getOriginalFilename()));
+        Files.createDirectories(filePath.getParent());
+        Files.deleteIfExists(filePath);
+
+        try (InputStream is = file.getInputStream();
+             OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
+             BufferedInputStream bis = new BufferedInputStream(is, 1024);
+             BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+        ) {
+            bis.transferTo(bos);
+        }
+
+        Avatar avatar = avatarRepository.findByStudentId(studentId).orElseGet(Avatar::new);
+        avatar.setStudent(student);
+        avatar.setFilePath(filePath.toString());
+        avatar.setFileSize(file.getSize());
+        avatar.setMediaType(file.getContentType());
+        avatar.setData(file.getBytes());
+
+        avatarRepository.save(avatar);
+    }
+
+    private String getExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
 }
